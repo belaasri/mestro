eb8b12e3342f7a9decc47a2aa73e3619e1a225bc
belaasri <107143805+belaasri@users.noreply.github.com>
2024-10-17 14:52:46 +0100

    Create pages.tsx

---
+++ pages.tsx
@@ -0,0 +1,9 @@
+import YouTubeDownloader from '../components/YouTubeDownloader'
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gray-100 py-12">
+      <YouTubeDownloader />
+    </main>
+  )
+}
